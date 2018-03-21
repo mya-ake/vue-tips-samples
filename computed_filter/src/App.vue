e2bdeb8b0d142cae34862c1c3105e29cec2d8a30
mya-ake <make.app.ts@gmail.com>
2018-03-21 18:34:47 +0900

    Add computed example

--- computed_filter/src/App.vue
+++ computed_filter/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <header>
+      <h1>computed と filter</h1>
+    </header>
+
+    <section>
+      <h2>computed</h2>
+      <computed-example/>
+    </section>
+
+    <section>
+      <h2>filter</h2>
+    </section>
+
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import ComputedExample from "./components/ComputedExample";
 export default {
-  name: "app",
   components: {
-    HelloWorld
+    ComputedExample
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ computed_filter/src/components/ComputedExample.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <em>タグの配列</em>
+      <ul>
+        <li
+          v-for="(tag, index) in tags"
+          v-bind:key="`tag-${index}`"
+        >{{ tag }}</li>
+      </ul>
+    </div>
+
+    <div>
+      <em>タグの文字列</em>
+      <p>{{ tagsString }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tags: ["JavaScript", "Vue.js", "computed"]
+    };
+  },
+
+  computed: {
+    tagsString() {
+      return this.tags.join("、");
+    }
+  }
+};
+</script>
