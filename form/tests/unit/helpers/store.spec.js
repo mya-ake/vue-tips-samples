42a72042d02635c2df5a07fae3cb47c581588e9a
mya-ake <make.app.ts@gmail.com>
2018-04-07 16:17:48 +0900

    Add store helper

---
+++ form/tests/unit/helpers/store.spec.js
@@ -0,0 +1,16 @@
+import * as store from "@/helpers/store";
+
+describe("Store helpers", () => {
+  it("buildModuleTypes", () => {
+    const moduleName = "user";
+    const types = {
+      SET_NAME: "SET_NAME",
+      SET_EMAIL: "SET_EMAIL"
+    };
+
+    const moduleTypes = store.buildModuleTypes({ moduleName, types });
+
+    expect(moduleTypes.SET_NAME).toBe("user/SET_NAME");
+    expect(moduleTypes.SET_EMAIL).toBe("user/SET_EMAIL");
+  });
+});
