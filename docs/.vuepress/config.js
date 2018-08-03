9116ad1ee380918c607ea5debd44da6473f8a07f
mya-ake <make.app.ts@gmail.com>
2018-08-03 20:11:02 +0900

    feat: サポートページのベース作った #11

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: '現場で使えるVue.js tips集のサポートページ',
+      description: '現場で使えるVue.js tips集のサポートページです。書籍内で書ききれなかった解説などが書かれる予定です。',
+    }
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+    sidebar: [],
+  }
+}
