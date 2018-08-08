7f6926bb512c4d8569bd93279bc4576b076819d8
mya-ake <make.app.ts@gmail.com>
2018-08-09 01:50:20 +0900

    refactor: スタイル調整

--- docs/.vuepress/views/DemoVuexTransition.vue
+++ docs/.vuepress/views/DemoVuexTransition.vue
@@ -1,8 +1,10 @@
 <template>
   <layout-demo>
-    <div>
+    <div class="nav">
       <router-link to="/">Home</router-link> | 
-      <router-link to="/demo/">Demo</router-link> | 
+      <router-link to="/demo/">Demo</router-link>
+    </div>
+    <div class="nav">
       <router-link to="/demo/vuex_transition_problem/">サンプルのHome</router-link> | 
       <router-link to="/demo/vuex_transition_problem/problem/asc">問題になる方</router-link> | 
       <router-link to="/demo/vuex_transition_problem/expect/asc">期待する方</router-link>
@@ -16,3 +18,10 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+.nav {
+  padding: 8px 0;
+}
+</style>
+
